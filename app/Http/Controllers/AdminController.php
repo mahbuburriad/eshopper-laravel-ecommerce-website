3d93dc448b4bdfa37e22eb9e8e9d9187c3c1eec0
mahbuburriad <mahbubur.riad@northsouth.edu>
2018-11-23 04:25:37 +0600

    admin controller added and content of the main css go to dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function index(){
-        return view ('admin.admin_login');
+        return view('admin.login'); 
     }
 }
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -17,10 +17,10 @@
 	<!-- end: Mobile Specific -->
 	
 	<!-- start: CSS -->
-	<link id="bootstrap-style" href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-	<link id="base-style" href="css/style.css" rel="stylesheet">
-	<link id="base-style-responsive" href="css/style-responsive.css" rel="stylesheet">
+	<link id="bootstrap-style" href="dashboard/css/bootstrap.min.css" rel="stylesheet">
+	<link href="dashboard/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link id="base-style" href="dashboard/css/style.css" rel="stylesheet">
+	<link id="base-style-responsive" href="dashboard/css/style-responsive.css" rel="stylesheet">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;subset=latin,cyrillic-ext,latin-ext' rel='stylesheet' type='text/css'>
 	<!-- end: CSS -->
 	
@@ -36,11 +36,11 @@
 	<![endif]-->
 		
 	<!-- start: Favicon -->
-	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="shortcut icon" href="dashboard/img/favicon.ico">
 	<!-- end: Favicon -->
 	
 			<style type="text/css">
-			body { background: url(img/bg-login.jpg) !important; }
+			body { background: url(dashboard/img/bg-login.jpg) !important; }
 		</style>
 		
 		
@@ -54,7 +54,7 @@
 			<div class="row-fluid">
 				<div class="login-box">
 					<div class="icons">
-						<a href="index.html"><i class="halflings-icon home"></i></a>
+						<a href="dashboard/index.html"><i class="halflings-icon home"></i></a>
 						<a href="#"><i class="halflings-icon cog"></i></a>
 					</div>
 					<h2>Login to your account</h2>
@@ -84,7 +84,8 @@
 					<h3>Forgot Password?</h3>
 					<p>
 						No problem, <a href="#">click here</a> to get a new password.
-					</p>	
+					</p>
+					
 				</div><!--/span-->
 			</div><!--/row-->
 			
@@ -92,63 +93,65 @@
 	</div><!--/.fluid-container-->
 	
 		</div><!--/fluid-row-->
+		
 	
 	<!-- start: JavaScript-->
 
-		<script src="js/jquery-1.9.1.min.js"></script>
-	<script src="js/jquery-migrate-1.0.0.min.js"></script>
+		<script src="dashboard/js/jquery-1.9.1.min.js"></script>
+	<script src="dashboard/js/jquery-migrate-1.0.0.min.js"></script>
 	
-		<script src="js/jquery-ui-1.10.0.custom.min.js"></script>
+		<script src="dashboard/js/jquery-ui-1.10.0.custom.min.js"></script>
 	
-		<script src="js/jquery.ui.touch-punch.js"></script>
+		<script src="dashboard/js/jquery.ui.touch-punch.js"></script>
 	
-		<script src="js/modernizr.js"></script>
+		<script src="dashboard/js/modernizr.js"></script>
 	
-		<script src="js/bootstrap.min.js"></script>
+		<script src="dashboard/js/bootstrap.min.js"></script>
 	
-		<script src="js/jquery.cookie.js"></script>
+		<script src="dashboard/js/jquery.cookie.js"></script>
 	
-		<script src='js/fullcalendar.min.js'></script>
+		<script src='dashboard/js/fullcalendar.min.js'></script>
 	
-		<script src='js/jquery.dataTables.min.js'></script>
+		<script src='dashboard/js/jquery.dataTables.min.js'></script>
 
-		<script src="js/excanvas.js"></script>
-	<script src="js/jquery.flot.js"></script>
-	<script src="js/jquery.flot.pie.js"></script>
-	<script src="js/jquery.flot.stack.js"></script>
-	<script src="js/jquery.flot.resize.min.js"></script>
+		<script src="dashboard/js/excanvas.js"></script>
+	<script src="dashboard/js/jquery.flot.js"></script>
+	<script src="dashboard/dashboard/js/jquery.flot.pie.js"></script>
+	<script src="dashboard/js/jquery.flot.stack.js"></script>
+	<script src="dashboard/js/jquery.flot.resize.min.js"></script>
 	
-		<script src="js/jquery.chosen.min.js"></script>
+		<script src="dashboard/js/jquery.chosen.min.js"></script>
 	
-		<script src="js/jquery.uniform.min.js"></script>
+		<script src="dashboard/js/jquery.uniform.min.js"></script>
 		
-		<script src="js/jquery.cleditor.min.js"></script>
+		<script src="dashboard/js/jquery.cleditor.min.js"></script>
 	
-		<script src="js/jquery.noty.js"></script>
+		<script src="dashboard/js/jquery.noty.js"></script>
 	
-		<script src="js/jquery.elfinder.min.js"></script>
+		<script src="dashboard/js/jquery.elfinder.min.js"></script>
 	
-		<script src="js/jquery.raty.min.js"></script>
+		<script src="dashboard/js/jquery.raty.min.js"></script>
 	
-		<script src="js/jquery.iphone.toggle.js"></script>
+		<script src="dashboard/js/jquery.iphone.toggle.js"></script>
 	
-		<script src="js/jquery.uploadify-3.1.min.js"></script>
+		<script src="dashboard/js/jquery.uploadify-3.1.min.js"></script>
 	
-		<script src="js/jquery.gritter.min.js"></script>
+		<script src="dashboard/js/jquery.gritter.min.js"></script>
 	
-		<script src="js/jquery.imagesloaded.js"></script>
+		<script src="dashboard/js/jquery.imagesloaded.js"></script>
 	
-		<script src="js/jquery.masonry.min.js"></script>
+		<script src="dashboard/js/jquery.masonry.min.js"></script>
 	
-		<script src="js/jquery.knob.modified.js"></script>
+		<script src="dashboard/js/jquery.knob.modified.js"></script>
 	
-		<script src="js/jquery.sparkline.min.js"></script>
+		<script src="dashboard/js/jquery.sparkline.min.js"></script>
 	
-		<script src="js/counter.js"></script>
+		<script src="dashboard/js/counter.js"></script>
 	
-		<script src="js/retina.js"></script>
+		<script src="dashboard/js/retina.js"></script>
 
-		<script src="js/custom.js"></script>
+		<script src="dashboard/js/custom.js"></script>
+    
 	<!-- end: JavaScript-->
 	
 </body>
